f150ffd4efa46e5573f39c4526fbbe29435404bf
manjulka <raketchica0@gmail.com>
2016-06-14 22:54:35 +0300

    CHANGEEE

--- src/animal_1/Animal.java
+++ src/animal_1/Animal.java
@@ -1,7 +1,6 @@
 package animal_1;
 
-import  animal_1.Food;
-
+//import class Food;
 /**
  * Created by inga on 22.05.2016.
  */
@@ -9,13 +8,22 @@ public abstract class Animal
 {
     private String name;
     private int weight;
+    public Animal(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
     public void eat(int foodWeight)
     {
         weight=+foodWeight;
 
     }
+    public String getName(){
+        return name;
+    }
+
     public void eat(Food food)
     {
         weight+=food.weight;
     }
 }
+//chANGE
